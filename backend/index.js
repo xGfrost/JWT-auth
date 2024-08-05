2d09d7306dbcf4d4a3339ef03e2b6d57ba776db0
xGfrost <fadelakbar528@gmail.com>
2024-08-06 00:33:45 +0700

    push 8

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ try {
     console.error(error);
 }
 
-app.use(cors({credentials:true, origin:'http://lcoalhost:3000'}));
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
