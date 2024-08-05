2afde1cf5376e68309801d53a6dcfee27b3a5b53
xGfrost <fadelakbar528@gmail.com>
2024-08-05 20:54:09 +0700

    push 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-  return (
-    <div>
-     <Login/>
-    </div>
-  );
+return (
+  <Router>
+    <Routes>
+      <Route exact path="/" Component={Login} />
+      <Route path="/register" Component={Register} />
+    </Routes>
+  </Router>
+);
 }
 
 export default App;
---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 className='title'>Welcome Back: </h1>
+    </div>
+  )
+}
+
+export default Dashboard
