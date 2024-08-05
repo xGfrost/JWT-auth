c1a9ef9644ee693c49a4da33508c09cf125c5e05
xGfrost <fadelakbar528@gmail.com>
2024-08-05 19:09:37 +0700

    push 3

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import db from "./config/database.js";
 import router from "./routes/index.js";
 // import Users from "./models/Usermodel.js";
@@ -16,6 +17,7 @@ try {
     console.error(error);
 }
 
+app.use(cors({credentials:true, origin:'http://lcoalhost:3000'}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
